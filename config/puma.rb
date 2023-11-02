0f3b1756cef988eb929928b068cc2fe15a411353
NikkiZapuiz <ma.nikki.zapuiz@gmail.com>
2023-11-02 17:13:01 +0800

    updated production builds

--- config/puma.rb
+++ config/puma.rb
@@ -12,11 +12,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") {4})
-  workers worker_count if worker_count > 1
-end
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 preload_app!
 
